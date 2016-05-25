7fa7e1af8cadff252402c89b0ff5b44627c2ccd1
Bedartha Goswami <bedartha@gmail.com>
2016-05-25 22:11:43 +0200

    Added __init__ file

---
+++ __init__.py
@@ -0,0 +1,47 @@
+"""
+Package bpl.
+
+Python module to work with bounded power-law (BPL) distributed random variables.
+
+This module contains a few basic functions that help analyse power-law
+distributed random variables that can be either bounded only from below or
+from both above and below. It provides the following functions:
+
+1.  Generate random samples from power-law distributed random variables.
+2.  Estimate a logarithimically binned histogram for a power-law
+    distributed random variable.
+3.  Estimate the probability density function and the cumulative
+    distribution function for specified power-law exponent.
+
+The latest version of this package is available at
+https://github.com/UP-RS-ESP/bpl
+
+For detailed documentation, please visit
+http://up-rs-esp.github.io/bpl
+
+"""
+# Created: Wed May 25, 2016  10:09PM
+# Last modified: Wed May 25, 2016  10:10PM
+# Copyright: Bedartha Goswami <goswami@uni-potsdam.de>
+
+
+__name__ = "bpl"
+"""The project name."""
+
+__author__ = "Bedartha Goswami <goswami@uni-potsdam.de>"
+"""The primary author of pygeonetwork."""
+
+__copyright__ = "Copyright (c) 2016 Bedartha Goswami."
+"""The copyright holder of this package 'bpl'."""
+
+__license__ = "GNU General Public License v2"
+"""The license governing the use and distribution of this module."""
+
+__url__ = "https://github.com/UP-RS-ESP/bpl"
+"""The URL for the package's homepage."""
+
+__version__ = 1.0
+"""Version number."""
+
+__date__ = "2016-05-25"
+"""The release date of this version."""
